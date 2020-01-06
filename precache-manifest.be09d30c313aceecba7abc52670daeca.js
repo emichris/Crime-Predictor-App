79b711bc8e7eda55f002981a0f2aa6c0e9b19ef7
Christian Emiyah <chemi1@morgan.edu>
2020-01-06 11:10:12 -0500

    Updates

---
+++ precache-manifest.be09d30c313aceecba7abc52670daeca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbd540b6735bd5676f26c13cdae47a3",
+    "url": "/LA-Crime-Predictor/index.html"
+  },
+  {
+    "revision": "80130b5f22bccfd0cd83",
+    "url": "/LA-Crime-Predictor/static/css/main.6806f2bb.chunk.css"
+  },
+  {
+    "revision": "dc2f08bce9da5771f837",
+    "url": "/LA-Crime-Predictor/static/js/2.11caf371.chunk.js"
+  },
+  {
+    "revision": "80130b5f22bccfd0cd83",
+    "url": "/LA-Crime-Predictor/static/js/main.8e08a2d5.chunk.js"
+  },
+  {
+    "revision": "67f855a275ba815e71ac",
+    "url": "/LA-Crime-Predictor/static/js/runtime-main.6263ccc4.js"
+  },
+  {
+    "revision": "63ebbabb732d870411dba5793f75293c",
+    "url": "/LA-Crime-Predictor/static/media/LA-city1.63ebbabb.jpg"
+  },
+  {
+    "revision": "fe62e76d00aa14139b24c7c496f41fe8",
+    "url": "/LA-Crime-Predictor/static/media/plot.fe62e76d.svg"
+  }
+]);
